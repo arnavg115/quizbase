6e62860b0b67bcc0c42c4874c9e8bead51844c73
Arnav G <arnavg286@gamil.com>
2021-09-11 17:53:41 -0700

    many changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,46 +2,120 @@ import { User } from "@supabase/gotrue-js";
 import type { NextPage } from "next";
 import { useEffect, useState } from "react";
 import { supabase } from "../utils/createClient";
+import { v4 } from "uuid";
 import Router from "next/router";
 const Home: NextPage = () => {
   const [User, setUser] = useState<User>();
+  const [deck, setDeck] = useState("");
+  const [loads, setLoads] = useState<any[]>([]);
   useEffect(() => {
     loadUser();
   }, []);
   const loadUser = async () => {
     const fo = await supabase.auth.user();
+    if (fo) {
+      const { data } = await supabase
+        .from("decks")
+        .select("*")
+        .eq("uid", fo.id);
+      setLoads(data!);
+    }
     setUser(fo!);
     const {} = await supabase.auth.onAuthStateChange(async () => {
       const user = await supabase.auth.user();
       setUser(user!);
+      await fetchThem();
     });
+  };
+  const fetchThem = async () => {
+    console.log(User);
+    if (User) {
+      const { data } = await supabase
+        .from("decks")
+        .select("*")
+        .eq("uid", User.id);
+      setLoads(data!);
+    }
   };
   return (
     <div>
-      <p>{User?.email}</p>
-      <p>Hello {User?.user_metadata.user_name}</p>
-      <img
-        src={User?.user_metadata.avatar_url}
-        style={{ width: "100px", borderRadius: "50%" }}
-      />
-      {User == undefined ? (
-        <button
-          onClick={() => {
-            Router.push("/auth");
-          }}
-        >
-          Sign In
-        </button>
+      {User ? (
+        <div>
+          <div style={{ display: "flex" }}>
+            <p>Signed in as {User.email}</p>
+
+            <img
+              src={User.user_metadata.avatar_url}
+              alt="profile"
+              style={{
+                width: "50px",
+                borderRadius: "50%",
+                marginLeft: "10px",
+                marginRight: "10px",
+              }}
+            />
+            <button
+              onClick={async () => {
+                await supabase.auth.signOut();
+              }}
+            >
+              Logout
+            </button>
+          </div>
+          <input
+            placeholder="Deck Name"
+            value={deck}
+            onChange={(e) => {
+              setDeck(e.target.value);
+            }}
+          />
+          <button
+            onClick={async () => {
+              // console.log(User.id);
+              const { data, error } = await supabase.from("decks").insert([
+                {
+                  id: v4(),
+                  name: deck,
+                  uid: User.id,
+                },
+              ]);
+              console.log(data);
+              setDeck("");
+              fetchThem();
+            }}
+          >
+            add
+          </button>
+        </div>
       ) : (
-        <button
-          onClick={async () => {
-            await supabase.auth.signOut();
-            setUser(undefined);
-          }}
-        >
-          Sign Out
-        </button>
+        <div>
+          <p>Sign In</p>
+          <button
+            onClick={() => {
+              Router.push("/auth");
+            }}
+          >
+            Go to login
+          </button>
+        </div>
       )}
+      <h1>Decks</h1>
+      {loads.map((x) => {
+        return (
+          <div key={x.id}>
+            <p>{x.name}</p>
+            <div>{x.cards ? x.cards.map((y: any) => <p>{y}</p>) : null}</div>
+            <button
+              onClick={async () => {
+                await supabase.from("decks").delete().eq("id", x.id!);
+                fetchThem();
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 };
